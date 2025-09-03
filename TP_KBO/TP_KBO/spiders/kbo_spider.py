5f89a0354aedb55098c9c66e100f8d44f1d3d34e
Maxime-C77 <152292406+Maxime-C77@users.noreply.github.com>
2025-09-03 17:35:31 +0200

    spider kbo

--- TP_KBO/TP_KBO/spiders/kbo_spider.py
+++ TP_KBO/TP_KBO/spiders/kbo_spider.py
@@ -1,15 +1,19 @@
 import scrapy
 import csv
+import random
 
 class KboSpider(scrapy.Spider):
     name = "kbo_spider"
 
     def start_requests(self):
         with open("../KBO/enterprise.csv", newline="", encoding="utf-8") as f:
-            reader = csv.DictReader(f)
-            for row in reader:
+            reader = list(csv.DictReader(f))
+            # choisir 10 entreprises au hasard
+            sample = random.sample(reader, min(10, len(reader)))
+
+            for row in sample:
                 numero = row["EnterpriseNumber"].replace(".", "")  # enlever les points
-                url = f"https://kbopub.economie.fgov.be/kbopub/toonondernemingps.html?ondernemingsnummer={numero}"
+                url = f"https://kbopub.economie.fgov.be/kbopub/toonondernemingps.html?lang=fr&ondernemingsnummer={numero}"
                 yield scrapy.Request(
                     url,
                     callback=self.parse,
@@ -23,13 +27,5 @@ class KboSpider(scrapy.Spider):
         yield {
             "_id": numero,
             "numero": numero,
-            "generalites": response.css("div#generalities::text").getall(),
-            # "fonctions": response.css("div#functions::text").getall(),
-            # "capacites": response.css("div#capacities::text").getall(),
-            # "qualites": response.css("div#qualities::text").getall(),
-            # "autorisations": response.css("div#authorisations::text").getall(),
-            # "nace_codes": response.css("div#nace::text").getall(),
-            # "donnees_financieres": response.css("div#financial_data::text").getall(),
-            # "liens_entites": response.css("div#entity_links a::attr(href)").getall(),
-            # "liens_externes": response.css("div#external_links a::attr(href)").getall(),
+            "donnees": response.css("td.QL::text, td.RL::text").getall(),
         }
